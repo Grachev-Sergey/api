5614e9b7dba367f736da7d1b74f8beff783d70a6
Sergey Grachev <serg3i.grach@yandex.ru>
2022-10-16 22:46:52 +0300

    added book list

--- src/db/entity/Book.ts
+++ src/db/entity/Book.ts
@@ -5,53 +5,36 @@ export class Book {
   @PrimaryGeneratedColumn()
     id: number;
 
-  @Column({
-    type: 'varchar',
-  })
-    name: string;
+  @Column({ type: 'varchar' })
+    cover: string;
 
-  @Column({
-    type: 'varchar',
-  })
+  @Column({ type: 'varchar' })
+    title: string;
+
+  @Column({ type: 'varchar' })
     author: string;
 
-  @Column({
-    type: 'varchar',
-  })
+  @Column({ type: 'varchar' })
     description: string;
 
-  @Column({
-    type: 'varchar',
-  })
+  @Column({ type: 'varchar' })
     dateOfIssue: string;
 
-  @Column({
-    type: 'varchar',
-  })
-    genre: string;
+  @Column({ type: 'varchar' })
+    genre: string[];
 
-  @Column({
-    type: 'varchar',
-  })
+  @Column({ type: 'varchar', nullable: true })
     hardCoverPrice: string;
 
-  @Column({
-    type: 'varchar',
-  })
+  @Column({ type: 'varchar' })
     paperback: boolean;
 
-  @Column({
-    type: 'varchar',
-  })
+  @Column({ type: 'varchar', nullable: true })
     aperbackPrice: string;
 
-  @Column({
-    type: 'varchar',
-  })
+  @Column({ type: 'varchar', nullable: true })
   rating: string;
 
-  @Column({
-    type: 'varchar',
-  })
+  @Column({ type: 'varchar', nullable: true })
     comments: string;
 }
