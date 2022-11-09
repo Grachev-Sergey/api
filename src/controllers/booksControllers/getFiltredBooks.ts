0c5e24fcb61a5da7c0dfaa0e6547bb6393653ed8
Sergey Grachev <serg3i-grach@yandex.ru>
2022-11-09 19:26:58 +0300

    changed answer with filtered books

--- src/controllers/booksControllers/getFiltredBooks.ts
+++ src/controllers/booksControllers/getFiltredBooks.ts
@@ -1,12 +1,11 @@
 import type { RequestHandler } from 'express';
-// import { StatusCodes } from 'http-status-codes';
 import { repositorys } from '../../db';
 import type { Book } from '../../db/entitys/Book';
-// import { customError } from '../../utils/error/customError';
-// import { BOOKS_NOT_FOUND } from '../../utils/error/errorsText';
 
 type QueryType = {
   genre?: string;
+  minPrice?: string;
+  maxPrice?: string;
 };
 
 type ParamsType = Record<string, never>;
@@ -21,20 +20,22 @@ type HandlerType = RequestHandler<ParamsType, ResponseType, BodyType, QueryType>
 
 export const getFiltredBooks:HandlerType = async (req, res, next) => {
   try {
-    const { genre } = req.query;
-    // eslint-disable-next-line no-console
-    console.log(genre);
+    const { genre, minPrice, maxPrice } = req.query;
     if (!genre.length) {
       const books = await repositorys.bookRepository
         .createQueryBuilder('book')
+        .andWhere('book.hardCoverPrice BETWEEN :minPrice AND :maxPrice', { minPrice, maxPrice })
         .getMany();
       return res.json({ books });
     }
     const genreArr = genre.split(',');
+    // eslint-disable-next-line no-console
+    console.log(maxPrice, minPrice);
     const books = await repositorys.bookRepository
       .createQueryBuilder('book')
       .leftJoinAndSelect('book.genre', 'genre')
       .where('genre.name IN (:...genreArr)', { genreArr })
+      .andWhere('book.hardCoverPrice BETWEEN :minPrice AND :maxPrice', { minPrice, maxPrice })
       .getMany();
     return res.json({ books });
   } catch (err) {
